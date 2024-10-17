9a7e17f59436f9fba185c7398b90f1b5a215684a
tloyan <thomas.loyan@gmail.com>
2024-10-17 16:09:25 +0200

    add font size and color to css config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,19 @@
 import "./globals.css";
+import { Young_Serif, Outfit } from "next/font/google"
 import { jsonLd } from "./jsonLd";
 export { metadata } from './metadata'
+
+const young_serif = Young_Serif({
+  subsets: ["latin"],
+  weight: ['400'],
+  variable: "--font-young-serif"
+})
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ['400', '600', '700'],
+  variable: "--font-outfit"
+})
 
 export default function RootLayout({
   children,
@@ -10,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body
-        className={`antialiased h-full`}
+        className={`${young_serif.variable} ${outfit.variable} antialiased h-full`}
       >
         <script
           type="application/ld+json"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,28 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rose: {
+          800: "#7A284E",
+          50: "#FFF7FB"
+        },
+        stone: {
+          900: "#312E2C",
+          600: "#5F564D",
+          150: "#E3DDD7",
+          100: "#F3E5D7"
+        },
+        brown: {
+          800: "#854632"
+        },
+        white: "#FFFFFF"
+      },
+      fontFamily: {
+        sans: "var(--font-outfit)",
+        serif: "var(--font-young-serif)"
+      }
+    },
   },
   plugins: [],
 };
